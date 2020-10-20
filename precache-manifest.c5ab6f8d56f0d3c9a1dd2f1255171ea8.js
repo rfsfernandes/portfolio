eee77d57e6b84768c6fad0e577694ab20fb807de
Rodrigo Fernandes <rfernandes@digital-works.com>
2020-10-20 21:12:04 +0100

    Updates

---
+++ precache-manifest.c5ab6f8d56f0d3c9a1dd2f1255171ea8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90283f446b6e23ba6e18f332862020eb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6ab17aee4a302110419e",
+    "url": "/portfolio/static/css/2.9f99e50b.chunk.css"
+  },
+  {
+    "revision": "c64b8669e15d6c26f882",
+    "url": "/portfolio/static/css/main.322b01b7.chunk.css"
+  },
+  {
+    "revision": "6ab17aee4a302110419e",
+    "url": "/portfolio/static/js/2.2160f0de.chunk.js"
+  },
+  {
+    "revision": "1a0968dc243fc785d05284e154e37292",
+    "url": "/portfolio/static/js/2.2160f0de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64b8669e15d6c26f882",
+    "url": "/portfolio/static/js/main.c6d111df.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "23e23ef2a668e6a0a8e46a035234c761",
+    "url": "/portfolio/static/media/earth-2.23e23ef2.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "3d3484b6fa3feb729b897581fadd9ed1",
+    "url": "/portfolio/static/media/profile.3d3484b6.png"
+  }
+]);
