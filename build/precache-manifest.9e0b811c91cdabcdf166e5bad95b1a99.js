da3e7d359e6702c825736a5094d462cfe875077a
Rodrigo Fernandes <rfernandes@digital-works.com>
2021-03-28 18:39:25 +0100

    added new cv

---
+++ build/precache-manifest.9e0b811c91cdabcdf166e5bad95b1a99.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb0b2d117b92f0541fb3b0cdea6bcd5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "aec39efca7d48c5308c6",
+    "url": "/portfolio/static/css/2.9f99e50b.chunk.css"
+  },
+  {
+    "revision": "513cf1a162ebc616e580",
+    "url": "/portfolio/static/css/main.9c270628.chunk.css"
+  },
+  {
+    "revision": "aec39efca7d48c5308c6",
+    "url": "/portfolio/static/js/2.e0eaab26.chunk.js"
+  },
+  {
+    "revision": "27774f0db5d62abf6bab5c14ced182b2",
+    "url": "/portfolio/static/js/2.e0eaab26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "513cf1a162ebc616e580",
+    "url": "/portfolio/static/js/main.569411b3.chunk.js"
+  },
+  {
+    "revision": "ffaaddae2ce7a75895d2205359786952",
+    "url": "/portfolio/static/js/pdf.worker.entry.4844c7bf.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "/portfolio/static/js/pdf.worker.entry.4844c7bf.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "58b2704eb96f3467b21a71f695370095",
+    "url": "/portfolio/static/media/continente.58b2704e.jpg"
+  },
+  {
+    "revision": "0487be805d1dd2469cb84e1925627e67",
+    "url": "/portfolio/static/media/digitalworks.0487be80.png"
+  },
+  {
+    "revision": "23e23ef2a668e6a0a8e46a035234c761",
+    "url": "/portfolio/static/media/earth-2.23e23ef2.jpg"
+  },
+  {
+    "revision": "653d0179acd4fd02692931581d89d89a",
+    "url": "/portfolio/static/media/eventphotography.653d0179.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/portfolio/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "20e64aecf22b430f7bd09b27ea5f9a05",
+    "url": "/portfolio/static/media/ipb.20e64aec.png"
+  },
+  {
+    "revision": "3d3484b6fa3feb729b897581fadd9ed1",
+    "url": "/portfolio/static/media/profile.3d3484b6.png"
+  },
+  {
+    "revision": "06c70ab049aa826ece96236f69c78144",
+    "url": "/portfolio/static/media/udemy.06c70ab0.png"
+  }
+]);
